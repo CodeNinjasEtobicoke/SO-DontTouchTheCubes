c34476ae7e5f20c44b807bf0bd7ccb5872423346
CodeNinja <148917437+iam9116@users.noreply.github.com>
2024-03-05 18:23:47 -0500

    SO-DontTouchTheCubes

--- Assets/Scripts/MovingAndRotatingObjects.cs
+++ Assets/Scripts/MovingAndRotatingObjects.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MovingAndRotatingObjects : MonoBehaviour
 {
+    [Header("Default Movement Speed")]
+    public float moveSpeed = 10f;
+    [Header("Default Rotation Speed")]
+    public float roateSpeed = 50f;
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        transform.Rotate(Vector3.up * roateSpeed * Time.deltaTime);
     }
 
     // Update is called once per frame
